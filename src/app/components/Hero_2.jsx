2beae237ffcc511eea9daf6f38afbb0a259801e8
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-08-12 21:14:48 -0700

    Added layout for menu hero

---
+++ src/app/components/Hero_2.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from '../styles/Hero_2.module.css';
+
+const Hero2 = () => {
+  return (
+    <div className={styles.heroContainer}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.heroTitle}>MENU</h1>
+        <div className={styles.menuSection}>
+          <h2>TACOS</h2>
+          <ul>
+            <li>STEAK AND SHRIMP</li>
+            <li>GRILLED SHRIMP</li>
+            <li>BAJA TACO (SHRIMP OR FISH)</li>
+            <li>PULPO LOCO</li>
+            <li>ASADA TACO</li>
+          </ul>
+        </div>
+        <div className={styles.menuSection}>
+          <h2>CEVICHE</h2>
+          <ul>
+            <li>CEVICHE</li>
+            <li>CEVICHE WITH MANGO</li>
+          </ul>
+        </div>
+        <div className={styles.menuSection}>
+          <h2>FRIES</h2>
+          <ul>
+            <li>STEAK FRIES</li>
+            <li>STEAK AND SHRIMP FRIES</li>
+            <li>GRILLED SHRIMP FRIES</li>
+          </ul>
+        </div>
+        <div className={styles.menuSection}>
+          <h2>AGUAS FRESCA</h2>
+          <ul>
+            <li>MANGO PINEAPPLE</li>
+            <li>CUCUMBER LIME</li>
+            <li>STRAWBERRY HIBISCUS</li>
+            <li>HORCHATA</li>
+            <li>BERRY LEMONADE</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero2;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero1 from './components/Hero_1';  // Correct path
+import Hero2 from './components/Hero_2';
 
 
 export default function Page() {
@@ -8,6 +9,7 @@ export default function Page() {
     <div>
       <Header />
       <Hero1 />
+      <Hero2 />
       {/* You can add more sections or components here */}
     </div>
   );
