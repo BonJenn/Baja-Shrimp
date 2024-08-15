93793b3fcad5c987fdb9c036df447ed09b5a5d90
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-08-15 16:16:59 -0700

    Spacing changes on text hero

--- src/app/components/Hero_4_Text.jsx
+++ src/app/components/Hero_4_Text.jsx
@@ -6,10 +6,16 @@ const Hero4 = () => {
 
   return (
     <div className={styles.textSection}>
-      <p>
+      <p className={styles.paragraph}>
         This locally cherished family-owned truck pays homage to a culinary delight held in deep affection by numerous San Diegans: the Baja shrimp. 
-        Meticulously battered and impeccably fried, each bite delivers a symphony of textures, enhanced by a vibrant and invigorating slaw. Within the realm of the revered Mexican interpretation of this beloved seafood, the Baja Shrimp food truck stands as an unwavering authority. Whether nestled in a warm taco or atop a crisp tostada, a visit to this establishment consistently promises an experience of utmost satisfaction.
+        Meticulously battered and impeccably fried, each bite delivers a symphony of textures, enhanced by a vibrant and invigorating slaw.
       </p>
+      <p className={styles.paragraph}>
+        Within the realm of the revered Mexican interpretation of this beloved seafood, the Baja Shrimp food truck stands as an unwavering authority. Whether nestled in a warm taco or atop a crisp tostada, a visit to this establishment consistently promises an experience of utmost satisfaction.
+      </p>
+      <div className={styles.logoContainer}>
+        <img src="/images/baja_shrimp_logo.png" alt="The Baja Shrimp Logo" className={styles.logo} />
+      </div>
     </div>
   );
 };
