58f67f48b2ecfa759c27908c10a312c0d54190a6
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-08-20 13:49:53 -0700

    Removed links from footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,13 +6,9 @@ const Footer = () => {
       <div className={styles.container}>
         <p className={styles.copyright}>&copy; {new Date().getFullYear()} The Baja Shrimp. All rights reserved.</p>
         <div className={styles.links}>
-          <a href="#" className={styles.link}>Privacy Policy</a>
-          {'  '}
-          <a href="#" className={styles.link}>Terms of Service</a>
-          {'  '}
-          <a href="#" className={styles.link}>Contact Us</a>
+       
         </div>
-        <p>Web Design by <a href="https://bontent.vercel.app">Bontent</a></p>
+        <p>Web Design by <a href="https://bontent.vercel.app"><strong>Bontent</strong></a></p>
       </div>
     </footer>
   );
